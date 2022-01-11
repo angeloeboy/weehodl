dafffb58ff2df062b22b97a625d15b1b248a90f8
Angelo <angeloeboy10@gmail.com>
2022-01-11 22:59:51 +0800

    .

--- assets/js/load-more.js
+++ assets/js/load-more.js
@@ -3,7 +3,7 @@ $(".project-item").hide();
 $(".project-item").slice(0, 6).show();
 
 $(".show-more").click(() => {
-    $(".project-item:hidden").slice(0, 3).fadeIn();
+    $(".project-item:hidden").slice(0, 4).fadeIn();
     
     if( $(".project-item:hidden").length == 0 ){
         $(".show-more").fadeOut();
