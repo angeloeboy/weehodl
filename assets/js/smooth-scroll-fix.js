49a10c64efebe737cdf532852811b135c2a9ff2b
Angelo <angeloeboy10@gmail.com>
2022-01-20 20:13:44 +0800

    .

--- assets/js/smooth-scroll-fix.js
+++ assets/js/smooth-scroll-fix.js
@@ -1,34 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const scroller = document.querySelector('.scroller');
-// let bodyScrollBar = Scrollbar.init(document.body, { damping: 0.05, delegateTo: document });
-let bodyScrollBar = Scrollbar.init(scroller, { damping: 0.05, delegateTo: document });
+// const scroller = document.querySelector('.scroller');
+// // let bodyScrollBar = Scrollbar.init(document.body, { damping: 0.05, delegateTo: document });
+// let bodyScrollBar = Scrollbar.init(scroller, { damping: 0.05, delegateTo: document });
 
  
-bodyScrollBar.setPosition(0, 0);
-bodyScrollBar.track.xAxis.element.remove();
+// bodyScrollBar.setPosition(0, 0);
+// bodyScrollBar.track.xAxis.element.remove();
 
-ScrollTrigger.scrollerProxy(document.body, {
-  scrollTop(value) {
-    if (arguments.length) {
-      bodyScrollBar.scrollTop = value;
-    }
-    return bodyScrollBar.scrollTop;
-  }
-});
+// ScrollTrigger.scrollerProxy(document.body, {
+//   scrollTop(value) {
+//     if (arguments.length) {
+//       bodyScrollBar.scrollTop = value;
+//     }
+//     return bodyScrollBar.scrollTop;
+//   }
+// });
 
 
-bodyScrollBar.addListener(ScrollTrigger.update);
+// bodyScrollBar.addListener(ScrollTrigger.update);
 
-const fixedElem = document.querySelector('nav')
-// const scrollToTop = $(".scroll-to-top")
+// const fixedElem = document.querySelector('nav')
+// // const scrollToTop = $(".scroll-to-top")
 
-const infos = document.querySelector(".infos")
+// const infos = document.querySelector(".infos")
 
-bodyScrollBar.addListener(status => {
-  const { offset } = status
-  fixedElem.style.top = `${offset.y}px`;
-})
+// bodyScrollBar.addListener(status => {
+//   const { offset } = status
+//   fixedElem.style.top = `${offset.y}px`;
+// })
 
 
 
